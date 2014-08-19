a0a5d63d1274153f44ef697af5070e6ce3a5d5d3
Tabish <tabishiqbal@outlook.com>
2014-08-19 18:04:27 -0400

    Display all contacts

--- crm.rb
+++ crm.rb
@@ -7,5 +7,10 @@ get '/' do
 end
 
 get '/contacts' do
+	@contacts = []
+	@contacts << Contact.new("Julie", "Hache", "julie@bitmakerlabs.com", "Instructor")
+	@contacts << Contact.new("Will", "Richman", "will@bitmakerlabs.com", "Co-Founder")
+	@contacts << Contact.new("Chris", "Johnston", "chris@bitmakerlabs.com", "Instructor")
+	
 	erb :contacts
 end
