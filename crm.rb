9d6937175b06155d279ba922399890103c8cc503
Tabish <tabishiqbal@outlook.com>
2014-08-21 20:59:34 -0400

    added 404 page if id not found

--- crm.rb
+++ crm.rb
@@ -25,4 +25,12 @@ post '/contacts' do
 	redirect to('/contacts')
 end
 
-
+get '/contacts/:id' do
+	@contact = @@rolodex.find(params[:id].to_i)
+	
+	if @contact
+		erb :show_contact
+	else
+		raise Sinatra::NotFound
+	end
+end
