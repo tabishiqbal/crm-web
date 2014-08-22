caea788aa9ab40ce7b4cbe750b4ea04c702c3cf2
Tabish <tabishiqbal@outlook.com>
2014-08-22 11:27:35 -0400

    Added datamapper

--- crm.rb
+++ crm.rb
@@ -1,29 +1,35 @@
 require_relative 'contact'
 require_relative 'rolodex'
 require 'sinatra'
+require 'data_mapper'
+
+DataMapper.setup(:default, "sqlite3:database.sqlite3")
 
 @@rolodex = Rolodex.new
 
-@@rolodex.add_contact(Contact.new("Johnny", "Bravo", "johnny@bitmakerlabs.com", "Rockstar"))
 
 get '/' do 
 	@crm_app_name = "My CRM"
 	erb :index
 end
 
+
 get '/contacts' do
 	erb :contacts
 end
 
+
 get '/contacts/new' do 
 	erb :new_contact
 end
+
 
 post '/contacts' do 
 	new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
 	@@rolodex.add_contact(new_contact)
 	redirect to('/contacts')
 end
+
 
 get '/contacts/:id' do
 	@contact = @@rolodex.find(params[:id].to_i)
@@ -35,14 +41,17 @@ get '/contacts/:id' do
 	end
 end
 
+
 get '/contacts/:id/edit' do
 	@contact = @@rolodex.find(params[:id].to_i)
+
 	if @contact
 		erb :edit_contact
 	else
 		raise Sinatra::NotFound
 	end
 end
+
 
 put '/contacts/:id' do
 	@contact = @@rolodex.find(params[:id].to_i)
@@ -57,6 +66,7 @@ put '/contacts/:id' do
 		raise Sinatra::NotFound
 	end
 end
+
 
 delete '/contacts/:id' do
 	@contact = @@rolodex.find(params[:id].to_i)
