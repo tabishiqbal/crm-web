37ec40eb502c5eda47aabeed915e61b46ff446cc
Tabish <tabishiqbal@outlook.com>
2014-08-22 15:01:15 -0400

    crm.rb modified, got rid of rolodex, implemented database

--- crm.rb
+++ crm.rb
@@ -1,11 +1,22 @@
-require_relative 'contact'
-require_relative 'rolodex'
+
 require 'sinatra'
 require 'data_mapper'
 
 DataMapper.setup(:default, "sqlite3:database.sqlite3")
 
-@@rolodex = Rolodex.new
+class Contact
+	include DataMapper::Resource
+
+	property :id, Serial
+	property :first_name, String
+	property :last_name, String
+	property :email, String
+	property :note, String
+end
+
+DataMapper.finalize
+DataMapper.auto_upgrade!
+
 
 
 get '/' do 
@@ -15,6 +26,7 @@ end
 
 
 get '/contacts' do
+	@contacts = Contact.all
 	erb :contacts
 end
 
@@ -25,14 +37,14 @@ end
 
 
 post '/contacts' do 
-	new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
-	@@rolodex.add_contact(new_contact)
-	redirect to('/contacts')
+	@contact = Contact.create(:first_name => params[:first_name], :last_name => params[:last_name], :email => params[:email], :note => params[:note])
+	# redirect to('/contacts')
+	erb :show_contact
 end
 
 
 get '/contacts/:id' do
-	@contact = @@rolodex.find(params[:id].to_i)
+	@contact = Contact.get(params[:id].to_i)
 	
 	if @contact
 		erb :show_contact
@@ -43,7 +55,7 @@ end
 
 
 get '/contacts/:id/edit' do
-	@contact = @@rolodex.find(params[:id].to_i)
+	@contact = Contact.get(params[:id].to_i)
 
 	if @contact
 		erb :edit_contact
@@ -54,7 +66,7 @@ end
 
 
 put '/contacts/:id' do
-	@contact = @@rolodex.find(params[:id].to_i)
+	@contact = Contact.get(params[:id].to_i)
 	if @contact
 		@contact.first_name = params[:first_name]
 		@contact.last_name = params[:last_name]
@@ -69,9 +81,9 @@ end
 
 
 delete '/contacts/:id' do
-	@contact = @@rolodex.find(params[:id].to_i)
+	@contact = Contact.get(params[:id].to_i)
 	if @contact
-		@@rolodex.remove_contact(@contact)
+		@contact.destroy
 		redirect to('/contacts')
 	else
 		raise Sinatra::NotFound
