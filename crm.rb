386ad6611d37b6308b317e1f68d55deb57335c33
Tabish <tabishiqbal@outlook.com>
2014-08-21 21:17:33 -0400

    modified crm.rb to edit contact

--- crm.rb
+++ crm.rb
@@ -43,3 +43,17 @@ get '/contacts/:id/edit' do
 		raise Sinatra::NotFound
 	end
 end
+
+put '/contacts/:id' do
+	@contact = @@rolodex.find(params[:id].to_i)
+	if @contact
+		@contact.first_name = params[:first_name]
+		@contact.last_name = params[:last_name]
+		@contact.email = params[:email]
+		@contact.note = params[:note]
+
+		redirect to('/contacts')
+	else
+		raise Sinatra::NotFound
+	end
+end
